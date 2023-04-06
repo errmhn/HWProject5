a9ed76ce3ceefae5180004fd5a4c071bdf497b31
Edy Rodriguez <Edy Rodriguez@LAPTOP-QFB1AMLV>
2023-04-05 22:09:20 -0600

    Correcting grammar

--- src/demo5/Test5.java
+++ src/demo5/Test5.java
@@ -5,7 +5,7 @@ public class Test5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test 5....");
-		System.out.println("Project udpdated...");
+		System.out.println("Project updated...");
 	}
 
 }
